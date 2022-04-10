90ae2d01d64fdbd046a2ee8db03c54fa90dad2d7
r10725040 <r10725040@ntu.edu.tw>
2022-04-10 21:38:36 +0800

    heroku react

--- ntulost/apps.py
+++ ntulost/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
+import axios from 'axios';
 
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 
 class NtuLostConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
