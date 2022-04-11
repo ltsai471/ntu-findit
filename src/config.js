0feba8c0e69b9103b435ac932b47003597b473fe
r10725040 <r10725040@ntu.edu.tw>
2022-04-11 11:04:57 +0800

    react

--- src/config.js
+++
@@ -1,9 +0,0 @@
-// Color Code
-const themeColor = {
-    primary: "#468C65",
-    primaryLight: "#A3BFAA",
-    secondary: "#706E61",
-    secondaryLight: "#A6937C",
-    default: "#0D0D0D",
-};
-export default themeColor;
