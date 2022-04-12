192cc05f535e4f010d77e16d506b407767d61d56
r10725040 <r10725040@ntu.edu.tw>
2022-04-12 22:09:36 +0800

    heroku-wsgi-production

--- hw_project/wsgi.py
+++ hw_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hw_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hw_project.production_settings')
 
 application = Cling(get_wsgi_application())
