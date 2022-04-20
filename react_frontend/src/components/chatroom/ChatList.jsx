01f39a98a4836d49b61be4237b5adaf3239e00a7
R10725045 <r10725045@ntu.edu.tw>
2022-04-20 21:10:41 +0800

    add chatroom css

--- react_frontend/src/components/chatroom/ChatList.jsx
+++ react_frontend/src/components/chatroom/ChatList.jsx
@@ -16,13 +16,23 @@
 // }
 import * as React from 'react';
 import List from '@mui/material/List';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import ChatItem from "./ChatItem";
+import themeColor from "../../config.js";
 
 
 export default function ChatList() {
     return (
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-            <ChatItem />
-        </List>
+        <div>
+            <Stack direction="row" spacing={2} justifyContent="center">
+                <Button variant="contained" color="success">全部</Button>
+                <Button variant="contained" color="primary">遺失</Button>
+                <Button variant="contained" color="success">拾獲</Button>
+            </Stack>
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                <ChatItem />
+            </List>
+        </div>
     );
 }
--- react_frontend/src/components/chatroom/MessageItem.jsx
+++ react_frontend/src/components/chatroom/MessageItem.jsx
@@ -2,29 +2,42 @@ import React, { Component, useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Grid from "@mui/material/Grid";
 
 export default function MessageItem() {
     const [loading, setLoading] = useState(true);
     const [orders, setOrders] = useState([]);
     const msgStyle = {
         'background-color': 'light-blue',
-    };    
+    };
+
+    // const { fromMe, text} = this.props;
+    // return (
+    //   <div className={`message-item ${fromMe ? 'message-from-me' : 'message-from-other'}`}>
+    //     <span>{text}</span>
+    //   </div>
+    // );
 
     return (
         <div className="message-item message-from-other">
             <Box component="div" sx={{ width: '100vh' }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" sx={{ display: 'inline', width: '20%' }} />
-                <Typography variant="body1" gutterBottom sx={{ display: 'inline', width: '80%' }}>
-                    陳小花
-                </Typography>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={3}>
+                        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                    </Grid>
+                    <Grid item xs={12} md={9}>
+                        <Typography variant="body1" gutterBottom>
+                            陳小花
+                        </Typography>
+                    </Grid>
+                </Grid>
             </Box>
             <Typography variant="body1" component="div" gutterBottom style={msgStyle}>
                 對啊
             </Typography>
         </div>
+
+
         // <div className="message-item message-from-other">
         //     <span>試著</span>
         // </div>
